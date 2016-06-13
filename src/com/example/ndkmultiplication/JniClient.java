6fe065eda505517fd369a08d92473133bd05f429
wliuxingxiangyu <wliuxingxiangyu@163.com>
2016-06-13 11:15:13 +0800

    :java层接口

--- src/com/example/ndkmultiplication/JniClient.java
+++ src/com/example/ndkmultiplication/JniClient.java
@@ -1,6 +1,6 @@
 package com.example.ndkmultiplication;
 
-public class JniClient {
+public class JniClient {//java层接口
 	static public native String AddStr(String strA, String strB);
 	static public native int AddInt(int a, int b);
 	static public native int MultiInt(int a, int b);
